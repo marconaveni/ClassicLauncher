af09e9f6efcbd22f91ba1b344b7ebc3432014b27
Marco Naveni <marconaveni.mco@hotmail.com>
2023-08-16 23:32:50 -0300

    add class in cpp and add scroll index start

--- Source/ClassicLauncher/Private/LoopScrollBox.cpp
+++ Source/ClassicLauncher/Private/LoopScrollBox.cpp
@@ -2,4 +2,100 @@
 
 
 #include "LoopScrollBox.h"
+#include "ClassicFunctionLibrary.h"
+#include "GameData.h"
+#include "Card.h"
+#include "Components/Button.h"
 
+void ULoopScrollBox::NativeOnInitialized()
+{
+	Offset = 0.0f;
+	Speed = 30.0f;
+	Time = 0.0f;
+	IndexScroll = 0;
+	Move = 0;
+	ChildrenCount = 0;
+	InputDirection = EButtonsGame::NONE;
+	PositionOffsetFocus = 1;
+	IndexFocusCard = 0;
+	UnlockInput = true;
+
+	BtnClick->OnPressed.AddDynamic(this, &ULoopScrollBox::OnClickButton);
+
+	PrepareScrollBox();
+	Super::NativeOnInitialized();
+}
+
+void ULoopScrollBox::NativeTick(const FGeometry& MyGeometry, float InDeltaTime)
+{
+	SetAnimatedScrollOffset();
+	ScrollMovement();
+
+	Super::NativeTick(MyGeometry, InDeltaTime);
+}
+
+void ULoopScrollBox::ScrollMovement()
+{
+	OnScrollMovement();
+}
+
+void ULoopScrollBox::SetAnimatedScrollOffset()
+{
+	OnSetAnimatedScrollOffset();
+}
+
+void ULoopScrollBox::DirectionRight()
+{
+	OnDirectionRight();
+}
+
+void ULoopScrollBox::DirectionLeft()
+{
+	OnDirectionLeft();
+}
+
+void ULoopScrollBox::PrepareScrollBox()
+{
+	OnPrepareScrollBox();
+}
+
+void ULoopScrollBox::StartScrollTo(EButtonsGame LeftRight)
+{
+	EButtonsGame Input = LeftRight;
+	OnStartScrollTo(Input);
+}
+
+void ULoopScrollBox::CancelScroll()
+{
+	OnCancelScroll();
+}
+
+void ULoopScrollBox::SelectDirectionScroll()
+{
+	OnSelectDirectionScroll();
+}
+
+void ULoopScrollBox::OpenCard()
+{
+	OnOpenCard();
+}
+
+void ULoopScrollBox::SetValuesCard(UCard* Card, FString Players, bool Favorite)
+{
+}
+
+void ULoopScrollBox::OnClickButton()
+{
+}
+
+void ULoopScrollBox::GetCardReferences(int32 Index, UCard*& Left, UCard*& Right)
+{
+}
+
+void ULoopScrollBox::AddCardsHorizontalBox(TArray<FGameData> GameData, int32 IndexFocus)
+{
+}
+
+void ULoopScrollBox::AddImagesCards(UTexture2D* NewTexture, int32 Width, int32 Height, int32 Index)
+{
+}
--- Source/ClassicLauncher/Public/LoopScrollBox.h
+++ Source/ClassicLauncher/Public/LoopScrollBox.h
@@ -4,8 +4,11 @@
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
+#include "ClassicFunctionLibrary.h"
+#include "GameData.h"
 #include "LoopScrollBox.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FDelegateCard, int32, Index);
 /**
  * 
  */
@@ -16,4 +19,109 @@ class CLASSICLAUNCHER_API ULoopScrollBox : public UUserWidget
 
 public:
 
+	UPROPERTY(BlueprintAssignable, BlueprintCallable)
+	FDelegateCard GetCardIndex;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "LoopScrollBox|Subclass")
+	TSubclassOf<class UCard> CardClassReference;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "LoopScrollBox|Variables")
+	TArray<class UCard*> CardReferenceLeft;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "LoopScrollBox|Variables")
+	TArray<class UCard*> CardReferenceCenter;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "LoopScrollBox|Variables")
+	TArray<class UCard*> CardReferenceRight;
+
+protected:
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "LoopScrollBox|Variables")
+	float Offset;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "LoopScrollBox|Variables")
+	float Speed;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "LoopScrollBox|Variables")
+	float Time;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "LoopScrollBox|Variables")
+	int32 IndexScroll;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "LoopScrollBox|Variables")
+	int32 Move;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "LoopScrollBox|Variables")
+	int32 ChildrenCount;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "LoopScrollBox|Variables")
+	EButtonsGame InputDirection;
+
+public:
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "LoopScrollBox|Variables")
+	int32 PositionOffsetFocus;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "LoopScrollBox|Variables")
+	int32 IndexFocusCard;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "LoopScrollBox|Variables")
+	bool UnlockInput;
+
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	class UScrollBox* ScrollBox;
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	class UHorizontalBox* HorizontalBoxLeft;
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	class UHorizontalBox* HorizontalBoxCenter;
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	class UHorizontalBox* HorizontalBoxRight;
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	class UImage* SizeStrech;
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	class UButton* BtnClick;
+
+	virtual void NativeOnInitialized() override;
+	virtual void NativeTick(const FGeometry& MyGeometry, float InDeltaTime) override;
+
+	UFUNCTION(BlueprintCallable, Category = "LoopScrollBox|Functions")
+	void ScrollMovement();
+	UFUNCTION(BlueprintCallable, Category = "LoopScrollBox|Functions")
+	void SetAnimatedScrollOffset();
+	UFUNCTION(BlueprintCallable, Category = "LoopScrollBox|Functions")
+	void DirectionRight();
+	UFUNCTION(BlueprintCallable, Category = "LoopScrollBox|Functions")
+	void DirectionLeft();
+	UFUNCTION(BlueprintCallable, Category = "LoopScrollBox|Functions")
+	void PrepareScrollBox();
+	UFUNCTION(BlueprintCallable, Category = "LoopScrollBox|Functions")
+	void StartScrollTo(EButtonsGame LeftRight);
+	UFUNCTION(BlueprintCallable, Category = "LoopScrollBox|Functions")
+	void CancelScroll();
+	UFUNCTION(BlueprintCallable, Category = "LoopScrollBox|Functions")
+	void SelectDirectionScroll();
+	UFUNCTION(BlueprintCallable, Category = "LoopScrollBox|Functions")
+	void OpenCard();
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnScrollMovement();
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnSetAnimatedScrollOffset();
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnDirectionLeft();
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnDirectionRight();
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnPrepareScrollBox();
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnStartScrollTo(const EButtonsGame& LeftRight);
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnCancelScroll();
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnSelectDirectionScroll();
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnOpenCard();
+
+	UFUNCTION(BlueprintPure, Category = "LoopScrollBox|Functions")
+	void GetCardReferences(int32 Index, class UCard*& Left, class UCard*& Right);
+
+	UFUNCTION(BlueprintCallable, Category = "LoopScrollBox|Functions")
+	void AddCardsHorizontalBox( TArray<FGameData> GameData, int32 IndexFocus);
+
+	UFUNCTION(BlueprintCallable, Category = "LoopScrollBox|Functions")
+	void AddImagesCards(UTexture2D* NewTexture, int32 Width, int32 Height,int32 Index);
+
+	UFUNCTION(BlueprintCallable, Category = "LoopScrollBox|Functions")
+	void SetValuesCard(class UCard* Card, FString Players ,bool Favorite);
+	
+	void OnClickButton();
 };
