2c85286149cef69ee49521e79b9a7ff57942cbbc
Marco Naveni <marconaveni.mco@hotmail.com>
2025-02-04 11:32:57 -0300

    Added build script

---
+++ scripts/build.py
@@ -0,0 +1,32 @@
+import subprocess
+import os
+import sys
+
+help = '''
+OPTIONS:
+  -d, --debug               Compile developer mode Debug  
+  -r, --release             Compile Release mode Release  
+'''
+
+build = "build"
+path_workspace = os.getcwd() + "/" + build  
+build_type = "Debug"
+
+for arg in sys.argv:
+    argl = arg.lower()
+    if argl == "--debug" or argl == "-d":
+        build_type = "Debug" 
+    elif argl == "--release" or argl == "-r":
+        build_type = "Release" 
+    elif argl == "--help" or argl == "-h":
+        print(help)
+        exit(0)
+
+try:
+    subprocess.run(["cmake", "--build", path_workspace, "--config", build_type], check=True) # cmake --build path/build --config type
+except subprocess.CalledProcessError as e:
+    print(f"Error cmake \": {e}")
+    print("Please check your CMake installation.")
+    exit(1)
+
+
--- scripts/configure.py
+++ scripts/configure.py
@@ -3,11 +3,21 @@ import os
 import subprocess
 import sys
 
+help = '''
+OPTIONS:
+  -d, --debug               Enable developer mode (arg CMake -DCMAKE_BUILD_TYPE=Debug)  
+  -r, --release             Enable Release mode (arg CMake -DCMAKE_BUILD_TYPE=Release)  
+  -asan, --fsanitize        Tool to detect memory access error such as use-after-free and memory leaks (arg CMake -DCLASSIC_LAUNCHER_FSANITIZE=TRUE)  
+  -pi, --raspberrypi        defines specific settings for raspberrypi (arg CMake -DCLASSIC_RASPBERRY_PI=TRUE)  
+  -f, --flags               Enable warning/error flags (arg CMake -DCLASSIC_LAUNCHER_FLAGS=TRUE)  
+  -t, --theme1x             Force Theme 1x (arg CMake -DCLASSIC_FORCE_THEME_1X=TRUE)  
+'''
+
 if os.name == 'nt':
     import configureWin
 
 build = "build"
-path_workspace = os.getcwd() + "\\" + build  
+path_workspace = os.getcwd() + "/" + build  
 build_type = "Release"
 fsanitize = "-DCLASSIC_LAUNCHER_FSANITIZE=FALSE"
 raspberrypi = "-DCLASSIC_RASPBERRY_PI=FALSE"
@@ -21,7 +31,7 @@ for arg in sys.argv:
         build_type = "Debug" 
     elif argl == "--release" or argl == "-r":
         build_type = "Release" 
-    elif argl == "--fsanitize" or argl == "-s":
+    elif argl == "--fsanitize" or argl == "-asan":
         fsanitize = "-DCLASSIC_LAUNCHER_FSANITIZE=TRUE"
     elif argl == "--raspberrypi" or argl == "-pi":
         raspberrypi = "-DCLASSIC_RASPBERRY_PI=TRUE"
@@ -29,8 +39,9 @@ for arg in sys.argv:
         flags = "-DCLASSIC_LAUNCHER_FLAGS=TRUE"
     elif argl == "--theme1x" or argl == "-t":
         theme = "-DCLASSIC_FORCE_THEME_1X=TRUE"
-
-        
+    elif argl == "--help" or argl == "-h":
+        print(help)
+        exit(0)
 
 
 try:
